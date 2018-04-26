996f094709da65956e5ccff5af3de1ffa625481d
Vinicius <vinicius-omf@hotmail.com>
2018-04-26 10:16:48 -0300

    1

--- starwars.js
+++ starwars.js
@@ -3,3 +3,34 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+function buscarFilmes() {
+   
+
+  	$.ajax({
+		url: 'https://swapi.co/api/films/',
+		method: 'GET',
+		success: function(resposta){
+			atualizaLista(resposta);
+	}});
+
+}
+
+function atualizaLista(data) {
+    for(let i=0; i< data.count; i++){
+
+    	let urlep = data.results[i].url;
+    	let epid = "Episode " + data.results[i].episode_id;
+
+    	conteudo = "<li onclick='buscarIntro(" + urlep+ ")'>" + epid + "</li>"
+    	$("#movies ul").append(conteudo);
+    }
+}
+
+function buscarIntro(data) {
+	alert(data);
+}
+
+$(document).ready(
+	 buscarFilmes()
+
+);
